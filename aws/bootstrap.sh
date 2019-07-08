fcce9f9ea95ca041ff9185adda9b781ec04c83d0
teevi <webstd.design@gmail.com>
2019-07-08 16:29:16 +0300

    AWS initial bootstrap

---
+++ aws/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo yum install -y npm
+sudo yum install -y nodejs npm
+sudo yum install -y nodejs
+sudo yum install -y node
+sudo yum install -y epel-release
+sudo amazon-linux-extras install epel
+sudo amazon-linux-extras install -y epel
+sudo yum install -y npm nodejs
+npm install
+npm start
